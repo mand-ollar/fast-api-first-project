dde358f429b53372f7c56976ff3d1d3cdc13845c
mand-ollar <minsec9912@icloud.com>
2026-03-14 00:26:37 +0900

    add .env

---
+++ api/app/config.py
@@ -0,0 +1,12 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    auth_secret_key: str = Field(alias="AUTH_SECRET_KEY", default="TEST_KEY")
+    auth_token_life_day: int = Field(alias="AUTH_TOKEN_LIFE_DAY", default=1825)
+
+
+config: Config = Config()
